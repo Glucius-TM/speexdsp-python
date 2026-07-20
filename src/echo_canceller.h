8c511305b80d53420cae546bf886e6ee59ccf5bb
Glucius <84855046+Glucius-TM@users.noreply.github.com>
2026-07-20 22:51:19 +0200

    Switch core echo canceller API to raw int16 buffers

--- src/echo_canceller.h
+++ src/echo_canceller.h
@@ -1,7 +1,6 @@
 #ifndef __ECHO_CANCELLER_H__
 #define __ECHO_CANCELLER_H__
 
-#include <string>
 #include <stdint.h>
 
 class EchoCanceller
@@ -9,9 +8,7 @@ class EchoCanceller
 public:
     static EchoCanceller* create(int frame_size=256, int filter_length=2048, int sample_rate=16000, int mics=1, int speakers=1);
 
-    virtual std::string process(const std::string& near, const std::string& far) = 0;
-
-    // Reinitialize internal Speex state after a discontinuity.
+    virtual void process(const int16_t* near, const int16_t* far, int16_t* out) = 0;
     virtual void reset() = 0;
 
     virtual ~EchoCanceller() {}

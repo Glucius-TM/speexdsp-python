ca8a6b7a47b17dc11020de87c8c16cedf8f2ab46
Glucius <84855046+Glucius-TM@users.noreply.github.com>
2026-07-20 22:51:45 +0200

    Migrate build to pybind11

--- setup.py
+++ setup.py
@@ -1,54 +1,39 @@
 # -*- coding: utf-8 -*-
 
-from glob import glob
 from pathlib import Path
 
-from setuptools import Extension, setup
+from pybind11.setup_helpers import Pybind11Extension, build_ext
+from setuptools import setup
 
 
 README = Path('README.md').read_text(encoding='utf-8')
 
-include_dirs = ['src']
-libraries = ['speexdsp', 'stdc++']
-define_macros = []
-extra_compile_args = []
-
-sources = (
-    glob('src/echo_canceller.cpp') +
-    ['src/speexdsp.i']
-)
-
-swig_opts = (
-    ['-c++'] +
-    ['-I' + h for h in include_dirs]
-)
+ext_modules = [
+    Pybind11Extension(
+        name='speexdsp._speexdsp',
+        sources=['src/bindings.cpp', 'src/echo_canceller.cpp'],
+        include_dirs=['src'],
+        libraries=['speexdsp'],
+        cxx_std=14,
+    )
+]
 
 
 setup(
     name='speexdsp',
-    version='0.1.2',
+    version='0.2.0',
     description='Python bindings of speexdsp library',
     long_description=README,
     long_description_content_type='text/markdown',
     author='Yihui Xiong',
     author_email='yihui.xiong@hotmail.com',
     maintainer='Glucius-TM',
-    maintainer_email='yihui.xiong@hotmail.com',
     url='https://github.com/Glucius-TM/speexdsp-python',
     download_url='https://github.com/Glucius-TM/speexdsp-python',
     packages=['speexdsp'],
     package_dir={'speexdsp': 'src'},
-    ext_modules=[
-        Extension(
-            name='speexdsp._speexdsp',
-            sources=sources,
-            swig_opts=swig_opts,
-            include_dirs=include_dirs,
-            libraries=libraries,
-            define_macros=define_macros,
-            extra_compile_args=extra_compile_args,
-        )
-    ],
+    ext_modules=ext_modules,
+    cmdclass={'build_ext': build_ext},
     classifiers=[
         'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: BSD License',
@@ -60,4 +45,5 @@ setup(
     keywords=['speexdsp', 'acoustic echo cancellation'],
     platforms=['Linux'],
     python_requires='>=3.8',
+    install_requires=['numpy'],
 )

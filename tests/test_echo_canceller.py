7ab8e17c70fb8bcdefc9815b6e9ef56dd48f51a7
Glucius <84855046+Glucius-TM@users.noreply.github.com>
2026-07-21 15:55:37 +0200

    Update smoke test to use numpy buffers

--- tests/test_echo_canceller.py
+++ tests/test_echo_canceller.py
@@ -1,15 +1,15 @@
+import numpy as np
 
-import wave
-import sys
 from speexdsp import EchoCanceller
-import pytest
 
 
 def test_echo_canceller():
     frames = 64
-    filter_length = 256
-    echo_canceller = EchoCanceller.create(64, 256, 16000)
+    echo_canceller = EchoCanceller.create(frames, 256, 16000)
 
-    chunk = '\0\0' * frames
+    chunk = np.zeros(frames, dtype=np.int16)
     for _ in range(16):
         out = echo_canceller.process(chunk, chunk)
+        assert isinstance(out, np.ndarray)
+        assert out.dtype == np.int16
+        assert out.shape == (frames,)
